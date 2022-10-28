3e13c1c11f3de655064950d35e463ed7fa4c9467
SchilleNathan <nathan.schille@gmail.com>
2022-10-28 14:00:08 +0200

    création entité Game

---
+++ src/Entity/Game.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GameRepository::class)
+ */
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $has_multiplayer_mode;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isHasMultiplayerMode(): ?bool
+    {
+        return $this->has_multiplayer_mode;
+    }
+
+    public function setHasMultiplayerMode(bool $has_multiplayer_mode): self
+    {
+        $this->has_multiplayer_mode = $has_multiplayer_mode;
+
+        return $this;
+    }
+}
