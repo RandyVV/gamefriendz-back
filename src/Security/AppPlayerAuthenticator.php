7f7db23afe84f334c9c92538407f095cc85dede1
SchilleNathan <nathan.schille@gmail.com>
2022-11-08 12:05:11 +0100

    Création backoffice (crud et templates) / auth

--- src/Security/AppPlayerAuthenticator.php
+++ src/Security/AppPlayerAuthenticator.php
@@ -2,33 +2,61 @@
 
 namespace App\Security;
 
+use App\Entity\Player;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class AppPlayerAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
+        // si la requête comporte /login et est en post, on veut essayer d'authentifier
+        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): Passport
-    {
-        // TODO: Implement authenticate() method.
-    }
+    {   
+        $apiToken = $request->headers->get('X-AUTH-TOKEN');
+
+        if (null === $apiToken) {
+            // Le token header était vide, authentication éhoue et 401
+            // Code 401 "Unauthorized"
+            throw new CustomUserMessageAuthenticationException('No API token provided // pas de token, pas de chocolat');
+        }
+
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport(
+            new UserBadge($apiToken), 
+            new CustomCredentials(function($password, Player $player) {
+                dd($password, $player);
+            }, $password)
+        );
+    } 
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        // si validé / authentifié
+
+        return null;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        // i non authentifié
+
+        $interdiction = ['message' => strtr($exception->getMessageKey(), $exception->getMessageData())];
+
+        return new JsonResponse($interdiction, Response::HTTP_UNAUTHORIZED);
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
@@ -41,4 +69,5 @@ class AppPlayerAuthenticator extends AbstractAuthenticator
 //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
 //         */
 //    }
+
 }
