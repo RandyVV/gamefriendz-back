5c63330504a4356c46da08cf58478496d82541d3
RandyVV <randy.venant@gmail.com>
2022-11-18 01:43:49 +0100

    Améliorations formulaire Platform

--- src/Form/PlatformType.php
+++ src/Form/PlatformType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Platform;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,16 @@ class PlatformType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la console'
+            ])
+            ->add('slug', TextType::class, [
+                'label' => 'Slug'
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
         ;
     }
 
