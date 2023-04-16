68ba660263d202fd9dc20e99554715fab60f0303
RandyVV <randy.venant@gmail.com>
2023-04-16 15:33:33 +0200

    ajout de bouton retirer sur card jeu

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/player')]
